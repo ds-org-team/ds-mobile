d5fde14029b85a2c243ad863566cbec4816b5849
Cristian Ribeiro <fhr.cristian@gmail.com>
2021-11-26 11:42:42 -0300

    feat(icon): cria o component icon

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { createRestyleComponent, useTheme } from '@shopify/restyle';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { IconProps } from './interfaces';
+import { Theme } from '../../themes';
+import Box from '../Box';
+
+const Icon: React.FC<IconProps> = props => {
+  const { colors } = useTheme<Theme>();
+
+  return (
+    <Box
+      sz="xs"
+      mr="nano"
+      alignItems="center"
+      justifyContent="center"
+      borderRadius="circular"
+      backgroundColor="transparent"
+    >
+      <MaterialIcon color={colors['neutral-dark']} {...props} />
+    </Box>
+  );
+};
+
+export default createRestyleComponent<IconProps, Theme>([], Icon);
---
+++ src/components/Icon/interfaces.ts
@@ -0,0 +1,5 @@
+import { IconButtonProps } from 'react-native-vector-icons/Icon';
+
+export type IconProps = {
+  icon?: string;
+} & IconButtonProps;
