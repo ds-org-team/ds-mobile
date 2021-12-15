e5dce6bb17a7f71c8d0bfc5edaaef60e70dcef57
Paulo Gomes <paulo.rgomes22@gmail.com>
2021-12-15 10:31:58 -0300

    chore(bob): adicionando bob como  builder

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React, { useEffect, useState } from 'react';
 import { Pressable } from 'react-native';
 import Animated from 'react-native-reanimated';
