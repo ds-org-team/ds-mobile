0567d80b8d46eae2e8aea47a612d10909257eab3
Paulo Gomes <paulo.rgomes22@gmail.com>
2021-12-22 12:45:33 -0300

    chore(App): reditando import do gesture handler

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable camelcase */
-import 'react-native-gesture-handler';
 import React from 'react';
 import { Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
